8d3e82f67452cba06d08c81c87a84355afb9789a
Deepsh <claudio.roberto.santana.f@gmail.com>
2026-08-05 14:31:22 -0300

    A

---
+++ app/wura/layout.js
@@ -0,0 +1,15 @@
+import "../../app/globals.css"
+
+export const metadata = {
+  title: "Grupo Diamond | Ilê Àṣẹ Egbé Jàhári",
+  description:
+    "O melhor grupo para aprender sobre Òrìṣà. Acesso a conteúdo exclusivo, materiais didáticos, reuniões secretas e descontos vitálicios.",
+};
+
+export default function DiamondLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body className="h-screen">{children}</body>
+    </html>
+  );
+}
---
+++ app/wura/page.jsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export default function Diamond() {
+  redirect('https://jim.com/a/tr-group');
+}
